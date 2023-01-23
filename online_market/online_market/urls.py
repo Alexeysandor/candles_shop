44a3980d40a6bf00061d2e22dbdfcb5fe4b7273a
Алексей Кузьменко <103373686+Alexeysandor@users.noreply.github.com>
2023-01-23 17:39:09 +0300

    Исправил баг с url при авторизации

--- online_market/online_market/urls.py
+++ online_market/online_market/urls.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from django.conf import settings
 from django.urls import include, path
-from users.urls import reset_password
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
     path('', include('users.urls')),
-    path('', include(reset_password))
 ]
 
 if settings.DEBUG:
