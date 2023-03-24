e9ca244075877cfbf2090237155bfe976f80e2f1
Алексей Кузьменко <wowalexey@gmail.com>
2023-03-24 15:23:51 +0300

    поправил requirements.txt

--- online_market/online_market/settings.py
+++ online_market/online_market/settings.py
@@ -61,7 +61,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
-    'corsheaders',
     'rest_framework.authtoken',
     'rest_framework',
     'djoser',
@@ -73,7 +72,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -81,7 +79,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
-# CORS_ALLOWED_ORIGINS = [    "http://localhost:8080", "http://127.0.0.1:8080",]
+
 
 
 ROOT_URLCONF = 'online_market.urls'
